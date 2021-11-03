852fc6ca0329ecd5a4eb78a0783876d712f9109c
Amohamed96 <amohamed.rg@gmail.com>
2021-11-03 18:03:34 -0400

    Crosswords up and down and none

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = require("/Users/abdirahman/lighthouse/w2/d2/matrix_transposition.js")
+const wordSearch = (letters, word) => {    /// letters is the matrix and word is the match youre looking for
+    const horizontalJoin = letters.map(function(ls) {
+        return ls.join('');
+    });
+    
+    for (const line of horizontalJoin) {
+        if (line.includes(word)) {
+            return true; 
+        }
     }
-}
+///-------------------------------------------------
+    let matrix = transpose(letters);
+    const VerticalJoin = matrix.map(function(ls) {
+        return ls.join('');
+    });
+
+    for (const line of VerticalJoin) {
+        if (line.includes(word)) {
+            return true;   
+        }
+    }
+
+    return false;
+};
 
 module.exports = wordSearch
+
+//             [ [d,e,f,g,h] [a,b,c,g,h]  [d,f,d,t,r] [ f,r,a,n,k]  ]
+//                   map0           map1        map2          map3
+// result -->
+//horizontaljoin =  [ ["defgh"]   ["abcgh"]  ["dfdtr"]  ["frank"]  ]
